cd442a8a9a1bfade48f91babdadca821c9fdf810
aswany <asaid.megaci@yahoo.com>
2024-02-01 09:48:45 +0000

    chars_strings

--- _printf.c
+++ _printf.c
@@ -1,72 +1,53 @@
 #include "main.h"
 /**
- *_printf - Custom printf function with limited functionality
- *format: Format string containing conversion specifiers
- *Return: Number of characters printed (excluding the null byte)
+ * main : is entry point to print char & strings & %
+ *
+ * return : the numbers of printed items
+ *
  **/
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-	
-	va_start(args, format);
-	count = process_format(format, args);
-	va_end(args);
-	return (count);
-}
+	int i = 0;
 
-/**
- *process_format - Process the format string and handle conversions
- *@format: Format string containing conversion specifiers
- *@args: Variable arguments list
- *Return: Number of characters printed (excluding the null byte)
- **/
-int process_format(const char *format, va_list args)
-{
-	int count, i;
-	
-	count = 0
-		for (i = 0; format[i] != '\0'; i++)
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
 		{
-			if (format[i] != '%')
+			putchar(*format);
+			i++;
+		}
+		else
+		{
+			switch (*++format)
 			{
-				_putchar(format[i]);
-				count++;
-			}
-			else
-			{
-				count += handle_conversion(format[i + 1], args);
-				i++;
+				case 'c':
+					i += fprintf(stdout, "%c", va_arg(args, int));
+					break;
+				case 's':
+					i += fprintf(stdout, "%s", va_arg(args, char *));
+					break;
+				default:
+					putchar(*format);
+					i += 2;
+					break;
 			}
 		}
-	return (count);
-}
-
-/**
- *handle_conversion - Handle a specific conversion specifier
- *@specifier: The conversion specifier
- *@args: Variable arguments list
- *Return: Number of characters printed (excluding the null byte)
- **/
-int handle_conversion(char specifier, va_list args)
-{
-	switch (format[i + 1])
-	{
-		case 'c':
-			handle_char(args);
-			return (1);
-		case 's':
-			return (handle_string(args));
-		case '%':
-			_putchar('%');
-			return (1);
-		case 'd':
-			handle_decimal(args);
-		case 'i':
-			handle_integer(args);
-		default:
-			_putchar('%');
-			_putchar(specifier);
-			return (2);
+		format++;
 	}
+	va_end(args);
+	return (i);
+}
+int main()
+{
+printf("the value of char is %c\n", 'x');
+_printf("the value of char is %c\n", 'x');
+printf("the value of string is %s\n", "hello");
+_printf("the value of string is %s\n", "hello");
+printf("the value of percentage is %% \n");
+_printf("the value of percentage is %% \n");
+return (0);
 }
